aa331a92cf76c98a9cd96b4be7ce83409c1be06b
MinkyuLee <zx811@naver.com>
2021-12-15 16:27:41 +0900

    feat: 유저 도메인 JPA Repository 등록

---
+++ server/user/src/main/java/com/server/user/users/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.server.user.users.repository;
+
+import com.server.user.users.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
